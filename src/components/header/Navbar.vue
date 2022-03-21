639a74397553d22aea75dd7ef941645729b776ba
TIANYUZHOU <1183380395@qq.com>
2022-03-21 22:17:30 +0800

    新增登陆注册表单

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -36,8 +36,68 @@
       >
     </div>
     <div v-if="!this.$store.state.isLogin" class="button-right">
-      <el-button round type="info">注册</el-button>
-      <el-button round type="primary">登录</el-button>
+      <a-space>
+        <a-button type="primary" shape="round" size="large" @click="showModal">
+          注册
+        </a-button>
+        <a-button type="danger" shape="round" size="large" @click="showModal">
+          登录
+        </a-button>
+        <a-modal
+          title="注册/登录"
+          :visible="visible"
+          @ok="handleOk"
+          @cancel="handleCancel"
+        >
+          <!-- 表单 -->
+          <a-form-model
+            ref="ruleForm"
+            :model="ruleForm"
+            :rules="rules"
+            v-bind="layout"
+          >
+            <a-form-model-item has-feedback label="Password" prop="pass">
+              <a-input
+                v-model="ruleForm.pass"
+                type="password"
+                autocomplete="off"
+              />
+            </a-form-model-item>
+            <a-form-model-item has-feedback label="Confirm" prop="checkPass">
+              <a-input
+                v-model="ruleForm.checkPass"
+                type="password"
+                autocomplete="off"
+              />
+            </a-form-model-item>
+            <a-form-model-item has-feedback label="Age" prop="age">
+              <a-input v-model.number="ruleForm.age" />
+            </a-form-model-item>
+            <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+              <!-- <a-button type="primary" @click="submitForm('ruleForm')">
+                Submit
+              </a-button>
+              <a-button
+                style="margin-left: 10px"
+                @click="resetForm('ruleForm')"
+              >
+                Reset
+              </a-button> -->
+            </a-form-model-item>
+          </a-form-model>
+          <template slot="footer">
+            <a-button key="back" @click="resetForm('ruleForm')"> 重置 </a-button>
+            <a-button
+              key="submit"
+              type="primary"
+              :loading="loading"
+              @click="submitForm('ruleForm')"
+            >
+              提交
+            </a-button>
+          </template>
+        </a-modal>
+      </a-space>
     </div>
     <div v-if="this.$store.state.isLogin" class="button-right">
       <a-popover
@@ -100,7 +160,12 @@
             <a-button type="danger" shape="round"> 退出 </a-button>
           </div>
         </template>
-        <a-button type="link" shape="circle"> <a-avatar src="https://assets.leetcode-cn.com/aliyun-lc-upload/users/shu-sheng-40/avatar_1581912063.png" icon="user" /> </a-button>
+        <a-button type="link" shape="circle">
+          <a-avatar
+            src="https://assets.leetcode-cn.com/aliyun-lc-upload/users/shu-sheng-40/avatar_1581912063.png"
+            icon="user"
+          />
+        </a-button>
       </a-popover>
     </div>
   </div>
@@ -110,15 +175,107 @@
   export default {
     name: 'Navbar',
     data() {
+      // 表单配置
+      let checkPending
+      let checkAge = (rule, value, callback) => {
+        clearTimeout(checkPending)
+        if (!value) {
+          return callback(new Error('Please input the age'))
+        }
+        checkPending = setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('Please input digits'))
+          } else {
+            if (value < 18) {
+              callback(new Error('Age must be greater than 18'))
+            } else {
+              callback()
+            }
+          }
+        }, 1000)
+      }
+      let validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password'))
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass')
+          }
+          callback()
+        }
+      }
+      let validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password again'))
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error("Two inputs don't match!"))
+        } else {
+          callback()
+        }
+      }
       return {
-        visible: false, // 气泡卡片配置
+        visible: false, // 头像气泡卡片配置
         userName: 'TIANYUZHO',
+
+        // 登录/注册 对话框配置
+        ModalText: 'Content of the modal',
+        // visible: false,
+        confirmLoading: false,
+
+        // 表单配置
+        loading: false,
+        ruleForm: {
+          pass: '',
+          checkPass: '',
+          age: '',
+        },
+        rules: {
+          pass: [{ validator: validatePass, trigger: 'change' }],
+          checkPass: [{ validator: validatePass2, trigger: 'change' }],
+          age: [{ validator: checkAge, trigger: 'change' }],
+        },
+        layout: {
+          labelCol: { span: 4 },
+          wrapperCol: { span: 14 },
+        },
       }
     },
     methods: {
       // 改变vuex 中Navbar的id
       changeNavId(id) {
         this.$store.commit('ChangeNavId', id)
+      },
+      // 登录/注册的对话框配置
+      showModal() {
+        this.visible = true
+      },
+      handleOk() {
+        this.ModalText = 'The modal will be closed after two seconds'
+        this.loading = true;
+        setTimeout(() => {
+          this.visible = false
+          this.loading = false
+        }, 2000)
+      },
+      handleCancel() {
+        console.log('Clicked cancel button')
+        this.visible = false
+      },
+
+      // 表单配置
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.handleOk() // 对话框原有方法触发
+            alert('submit!')
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields()
       },
     },
   }
