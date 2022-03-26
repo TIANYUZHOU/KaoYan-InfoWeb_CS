46ca8dff8e4911ad3b631bd1ff02ca20665699cc
TIANYUZHOU <1183380395@qq.com>
2022-03-26 22:24:17 +0800

    拆分组件-注册登录表单/用户卡片

---
+++ src/components/header/RegisteAndLogin/RegisteAndLogin.vue
@@ -0,0 +1,519 @@
+<template>
+  <div v-if="!this.$store.state.isLogin" class="button-right">
+    <a-space>
+      <a-button
+        type="primary"
+        shape="round"
+        size="large"
+        @click="showModalRegiste"
+      >
+        注册
+      </a-button>
+      <a-button
+        type="danger"
+        shape="round"
+        size="large"
+        @click="showModalLogin"
+      >
+        登录
+      </a-button>
+      <a-modal :visible="visible" @ok="handleOk" @cancel="handleCancel"
+        ><a-card
+          style="width: 100%"
+          :bordered="false"
+          :tab-list="tabListNoTitle"
+          :active-tab-key="noTitleKey"
+          @tabChange="(key) => onTabChange(key, 'noTitleKey')"
+        >
+          <!--注册表单-->
+          <template v-if="noTitleKey === 'registe'">
+            <br />
+            <a-form-model
+              ref="ruleForm"
+              :model="ruleForm"
+              :rules="rules"
+              v-bind="layout"
+            >
+              <a-form-model-item has-feedback label="用户名" prop="name">
+                <a-input v-model="ruleForm.name" placeholder="请输入用户名" />
+              </a-form-model-item>
+              <a-form-model-item has-feedback label="密码" prop="pass">
+                <a-input
+                  v-model="ruleForm.pass"
+                  type="password"
+                  autocomplete="off"
+                  placeholder="请输入密码"
+                />
+              </a-form-model-item>
+              <a-form-model-item has-feedback label="确认密码" prop="checkPass">
+                <a-input
+                  v-model="ruleForm.checkPass"
+                  type="password"
+                  autocomplete="off"
+                  placeholder="请再次输入密码"
+                />
+              </a-form-model-item>
+              <a-form-model-item has-feedback label="手机号" prop="mobile">
+                <a-input
+                  autocomplete="true"
+                  v-model="ruleForm.mobile"
+                  placeholder="请输入手机号"
+                />
+              </a-form-model-item>
+              <a-form-model-item label="验证码" :wrapper-col="{ span: 8 }">
+                <a-input v-model="ruleForm.code" placeholder="请输入验证码" />
+              </a-form-model-item>
+              <a-form-model-item :wrapper-col="{ span: 4, offset: 4 }">
+                <a-button
+                  type="primary"
+                  :disabled="this.disabled"
+                  @click="sendcode"
+                >
+                  {{ this.btntxt }}
+                </a-button>
+              </a-form-model-item>
+            </a-form-model>
+          </template>
+          <!--登录表单-->
+          <template v-if="noTitleKey === 'login'">
+            <br />
+            <a-form-model
+              ref="ruleFormLogin"
+              :model="ruleFormLogin"
+              :rules="rulesLogin"
+              v-bind="layoutLogin"
+            >
+              <a-button
+                style="float: right; margin-right: -20px; margin-top: -40px"
+                type="danger"
+                size="small"
+                @click="changeLoginType"
+              >
+                切换登录方式
+                <a-icon type="right" />
+              </a-button>
+              <!-- 验证码登录 -->
+              <div v-if="this.isMobile">
+                <a-form-model-item
+                  has-feedback
+                  label="手机号"
+                  prop="mobileLogin"
+                >
+                  <a-input
+                    autocomplete="true"
+                    v-model="ruleFormLogin.mobileLogin"
+                    placeholder="请输入手机号"
+                  />
+                </a-form-model-item>
+                <a-form-model-item label="验证码" :wrapper-col="{ span: 8 }">
+                  <a-input
+                    v-model="ruleFormLogin.codeLogin"
+                    placeholder="请输入验证码"
+                  />
+                </a-form-model-item>
+                <a-form-model-item :wrapper-col="{ span: 4, offset: 4 }">
+                  <a-button
+                    type="primary"
+                    :disabled="this.disabledLogin"
+                    @click="sendcodeLogin"
+                  >
+                    {{ this.btntxtLogin }}
+                  </a-button>
+                </a-form-model-item>
+              </div>
+              <!-- 用户名密码登录 -->
+              <div v-if="!this.isMobile">
+                <a-form-model-item has-feedback label="用户名" prop="nameLogin">
+                  <a-input
+                    v-model="ruleFormLogin.nameLogin"
+                    placeholder="输入用户名或手机号"
+                  />
+                </a-form-model-item>
+                <a-form-model-item has-feedback label="密码" prop="passLogin">
+                  <a-input
+                    v-model="ruleFormLogin.passLogin"
+                    type="password"
+                    autocomplete="off"
+                    placeholder="请输入密码"
+                  />
+                </a-form-model-item>
+              </div>
+            </a-form-model>
+          </template>
+        </a-card>
+        <!-- 弹出框下方按钮-注册 -->
+        <template v-if="this.noTitleKey === 'registe'" slot="footer">
+          <a-button key="back" @click="resetForm('ruleForm')"> 重置 </a-button>
+          <a-button
+            key="submit"
+            type="primary"
+            :loading="loading"
+            @click="submitForm('ruleForm')"
+          >
+            提交
+          </a-button>
+        </template>
+        <!-- 弹出框下方按钮-登录 -->
+        <template v-if="this.noTitleKey === 'login'" slot="footer">
+          <a-button
+            key="login"
+            type="primary"
+            :loading="loading"
+            @click="submitForm('ruleFormLogin')"
+          >
+            登录
+          </a-button>
+        </template>
+      </a-modal>
+    </a-space>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'RegisteAndLogin',
+    data() {
+      // 表单配置
+      let validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'))
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass')
+          }
+          callback()
+        }
+      }
+      let validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'))
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不匹配'))
+        } else {
+          callback()
+        }
+      }
+      let validateName = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入用户名'))
+        } else {
+          callback()
+        }
+      }
+      let validateMobile = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入手机号'))
+        } else if (!/^0?1[3|4|5|7|8][0-9]\d{8}$/.test(value)) {
+          callback(new Error('请输入正确的手机号'))
+        } else {
+          callback()
+        }
+      }
+
+      let validateNameLogin = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入用户名'))
+        } else {
+          callback()
+        }
+      }
+
+      let validatePassLogin = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'))
+        } else {
+          // if (this.ruleForm.checkPass !== '') {
+          //   this.$refs.ruleForm.validateField('checkPass')
+          // }
+          callback()
+        }
+      }
+
+      let validateMobileLogin = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入手机号'))
+        } else if (!/^0?1[3|4|5|7|8][0-9]\d{8}$/.test(value)) {
+          callback(new Error('请输入正确的手机号'))
+        } else {
+          callback()
+        }
+      }
+      return {
+        visible: false,
+        confirmLoading: false,
+
+        // 卡片配置
+
+        tabListNoTitle: [
+          {
+            key: 'registe',
+            tab: '注册',
+          },
+          {
+            key: 'login',
+            tab: '登录',
+          },
+        ],
+        noTitleKey: 'login',
+        loading: false,
+
+        // 表单配置
+
+        // 注册表单
+        ruleForm: {
+          code: '',
+          pass: '',
+          checkPass: '',
+          mobile: '',
+          name: '',
+        },
+        rules: {
+          pass: [{ validator: validatePass, trigger: 'change' }],
+          checkPass: [{ validator: validatePass2, trigger: 'change' }],
+          name: [{ validator: validateName, trigger: 'change' }],
+          mobile: [{ validator: validateMobile, trigger: 'change' }],
+        },
+        layout: {
+          labelCol: { span: 4 },
+          wrapperCol: { span: 16 },
+        },
+
+        // 登录表单
+        ruleFormLogin: {
+          codeLogin: '',
+          nameLogin: '',
+          passLogin: '',
+          mobileLogin: '',
+        },
+        rulesLogin: {
+          nameLogin: [{ validator: validateNameLogin, trigger: 'change' }],
+          mobileLogin: [{ validator: validateMobileLogin, trigger: 'change' }],
+          passLogin: [{ validator: validatePassLogin, trigger: 'change' }],
+        },
+        layoutLogin: {
+          labelCol: { span: 4 },
+          wrapperCol: { span: 16 },
+        },
+        // 自定义表单配置
+        // 登录方式
+        isMobile: true,
+
+        // 获取验证码按钮
+        btntxt: '获取验证码',
+        disabled: false,
+        time: 0,
+        mobiledata: {}, // 返回的数据
+        btntxtLogin: '获取验证码',
+        disabledLogin: false,
+        timeLogin: 0,
+        mobiledataLogin: {}, // 返回的数据
+      }
+    },
+    methods: {
+      // 改变vuex 中的 isLogin
+      changeLoginState(bool) {
+        this.$store.commit('ChangeLoginState', bool)
+      },
+      // 将用户信息写入vuex
+      addUserInfo(user) {
+        this.$store.commit('AddUserInfo', user)
+      },
+      // 登录/注册的对话框配置
+      showModalRegiste() {
+        this.visible = true
+        this.noTitleKey = 'registe'
+      },
+      showModalLogin() {
+        this.visible = true
+        this.noTitleKey = 'login'
+      },
+      handleOk() {
+        this.ModalText = '对话框两秒后关闭！'
+        this.loading = true
+        setTimeout(() => {
+          this.visible = false
+          this.loading = false
+        }, 2000)
+      },
+      handleCancel() {
+        // console.log('点击取消按钮')
+        this.visible = false
+      },
+      // 卡片配置
+      onTabChange(key, type) {
+        // 清除表单校验结果
+        if (this.$refs.ruleForm) {
+          this.$refs.ruleForm.clearValidate()
+        }
+        if (this.$refs.ruleFormLogin) {
+          this.$refs.ruleFormLogin.clearValidate()
+        }
+        // console.log(key, type)
+        this[type] = key
+      },
+      // 表单配置
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            // 注册表单提交
+            if (formName === 'ruleForm') {
+              // console.log(this.ruleForm)
+              let data = {
+                username: this.ruleForm.name,
+                password: this.ruleForm.pass,
+                mobile: this.ruleForm.mobile,
+                sms_code: this.ruleForm.code,
+              }
+              let url = 'http://127.0.0.1:8000/users/'
+              axios
+                .post(url, data)
+                .then(() => {
+                  // console.log(res.data)
+                  this.handleOk() // 对话框原有方法触发
+                  alert('提交成功!')
+                })
+                .catch((e) => {
+                  if (e.response.data.non_field_errors) {
+                    alert('验证码错误！')
+                  } else if (e.response.data.mobile) {
+                    alert('手机号为：' + data.mobile + '的用户已存在')
+                  }
+                  // console.log(e.response.data)
+                })
+            }
+            // 登录表单提交
+            else if (formName === 'ruleFormLogin') {
+              let data = {
+                username: '',
+                password: '',
+              }
+              // 登录需要的用户信息
+              // 用户名或电话 都通过username字段传给后端
+              // 密码或验证码 都通过passwor字段传给后端
+              if (this.isMobile) {
+                data.username = this.ruleFormLogin.mobileLogin
+                data.password = this.ruleFormLogin.codeLogin
+              } else {
+                data.username = this.ruleFormLogin.nameLogin
+                data.password = this.ruleFormLogin.passLogin
+              }
+              let url = 'http://127.0.0.1:8000/login/'
+              axios
+                .post(url, data)
+                .then((res) => {
+                  // console.log(res.data)
+                  if (res.data.error) {
+                    alert(res.data.message)
+                  } else {
+                    localStorage.user_id = res.data.user_id
+                    localStorage.username = res.data.username
+                    localStorage.avatar = res.data.avatar
+                    localStorage.token = res.data.access
+                    // this.$store.state.isLogin = true
+                    this.addUserInfo(res.data)
+                    this.changeLoginState(true)
+                    this.handleOk() // 对话框原有方法触发
+                  }
+                })
+                .catch(() => {
+                  // console.log(e.response.data)
+                })
+            }
+          } else {
+            // console.log('提交失败!!')
+            return false
+          }
+        })
+      },
+      logout() {
+        window.localStorage.clear()
+        // this.$store.state.isLogin = false
+        this.changeLoginState(false)
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields()
+      },
+      // 自定义表单配置
+      changeLoginType() {
+        this.isMobile = !this.isMobile
+        // 清除表单校验结果
+        if (this.$refs.ruleForm) {
+          this.$refs.ruleForm.clearValidate()
+        }
+        if (this.$refs.ruleFormLogin) {
+          this.$refs.ruleFormLogin.clearValidate()
+        }
+      },
+
+      // 验证码按钮
+      //验证手机号码部分
+      sendcode: function () {
+        let url = ' http://127.0.0.1:8000/sms_code/' + this.ruleForm.mobile
+        if (this.ruleForm.mobile == '') {
+          alert('请输入手机号码')
+          return
+        } else if (!/^0?1[3|4|5|7|8][0-9]\d{8}$/.test(this.ruleForm.mobile)) {
+          return
+        }
+        this.time = 35
+        this.disabled = true
+        this.timer()
+        axios.get(url).then((res) => {
+          this.mobiledata = res.data
+          // console.log(this.mobiledata)
+          alert(this.mobiledata.SendStatusSet[0].Message)
+        })
+      },
+      sendcodeLogin: function () {
+        let url =
+          ' http://127.0.0.1:8000/sms_code/' + this.ruleFormLogin.mobileLogin
+        if (this.ruleFormLogin.mobileLogin == '') {
+          alert('请输入手机号码')
+          return
+        } else if (
+          !/^0?1[3|4|5|7|8][0-9]\d{8}$/.test(this.ruleFormLogin.mobileLogin)
+        ) {
+          return
+        }
+        this.timeLogin = 35
+        this.disabledLogin = true
+        this.timerLogin()
+        axios.get(url).then((res) => {
+          this.mobiledataLogin = res.data
+          // console.log(this.mobiledata)
+          alert(this.mobiledataLogin.SendStatusSet[0].Message)
+        })
+      },
+      timer: function () {
+        if (this.time > 0) {
+          this.time--
+          this.btntxt = this.time + 's,后重新发送'
+          setTimeout(this.timer, 1000)
+        } else {
+          this.time = 0
+          this.btntxt = '获取验证码'
+          this.disabled = false
+        }
+      },
+      timerLogin: function () {
+        if (this.timeLogin > 0) {
+          this.timeLogin--
+          this.btntxtLogin = this.timeLogin + 's,后重新发送'
+          setTimeout(this.timerLogin, 1000)
+        } else {
+          this.timeLogin = 0
+          this.btntxtLogin = '获取验证码'
+          this.disabledLogin = false
+        }
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .button-right {
+    float: right;
+    margin-right: 30px;
+  }
+</style>
---
+++ src/components/header/UserCard/UserCard.vue
@@ -0,0 +1,117 @@
+<template>
+  <div v-if="this.$store.state.isLogin" class="button-right">
+    <a-popover
+      v-model="visible"
+      :title="this.$store.state.userInfo.username + ' 的账户'"
+      trigger="click"
+    >
+      <template slot="content">
+        <div id="firdiv">
+          <div>
+            <a-tooltip>
+              <template slot="title"> 我的收藏 </template>
+              <a-button
+                style="background-color: #f8b595; border-color: #f8b595"
+                type="primary"
+                shape="circle"
+                icon="star"
+              />
+            </a-tooltip>
+          </div>
+          <div>
+            <a-tooltip>
+              <template slot="title"> 个人资料 </template>
+              <a-button
+                style="background-color: #f67280; border-color: #f67280"
+                type="primary"
+                shape="circle"
+                icon="file"
+              />
+            </a-tooltip>
+          </div>
+        </div>
+        <a-divider />
+        <div id="secdiv">
+          <div>
+            <a-tooltip>
+              <template slot="title"> 意见反馈 </template>
+              <a-button
+                style="background-color: #455d7a; border-color: #455d7a"
+                type="primary"
+                shape="circle"
+                icon="solution"
+              />
+            </a-tooltip>
+          </div>
+          <div>
+            <a-tooltip>
+              <template slot="title"> 关于 </template>
+              <a-button
+                style="background-color: #233142; border-color: #233142"
+                type="primary"
+                shape="circle"
+                icon="idcard"
+              />
+            </a-tooltip>
+          </div>
+        </div>
+        <a-divider />
+        <div id="thrdiv">
+          <a-button type="danger" shape="round" @click="logout">
+            退出
+          </a-button>
+        </div>
+      </template>
+      <a-button type="link" shape="circle">
+        <a-avatar :src="this.$store.state.userInfo.avatar" icon="user" />
+      </a-button>
+    </a-popover>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'UserCard',
+    data() {
+      return {
+        visible: false, // 头像气泡卡片配置
+      }
+    },
+    methods: {
+      logout() {
+        window.localStorage.clear()
+        // this.$store.state.isLogin = false
+        this.changeLoginState(false)
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  #firdiv {
+    display: flex;
+    justify-content: center;
+  }
+  #firdiv > div {
+    display: flex;
+    justify-content: center;
+    flex: 1;
+  }
+  #secdiv {
+    display: flex;
+    justify-content: center;
+  }
+  #secdiv > div {
+    display: flex;
+    justify-content: center;
+    flex: 1;
+  }
+  #thrdiv {
+    display: flex;
+    justify-content: center;
+  }
+  .button-right {
+    float: right;
+    margin-right: 30px;
+  }
+</style>
