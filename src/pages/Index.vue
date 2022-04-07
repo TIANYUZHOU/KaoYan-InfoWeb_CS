25746980174a020ac34bdb7624fedf2e8ccc87ac
TIANYUZHOU <1183380395@qq.com>
2022-04-07 23:10:38 +0800

    新增-首页

---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="background">
+    <a-result
+      title="欢迎进入『上岸吧』计算机考研信息查询系统。"
+      sub-title="接下来你可以点击右上方的“注册”按钮进行用户注册，或者多点击“登录”按钮进行登录操作。"
+    >
+      <template #icon>
+        <a-icon type="smile" theme="twoTone" />
+      </template>
+      <template #extra>
+        <a-popconfirm
+          title="赶快点击右上角登录吧！"
+          ok-text="好的"
+          cancel-text="取消"
+          placement="bottom"
+        >
+          <a href="#"><a-button type="primary"> 我知道了 </a-button></a>
+        </a-popconfirm>
+      </template>
+    </a-result>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Index',
+    data() {
+      return {}
+    },
+  }
+</script>
+
+<style scoped>
+  #background {
+    background-image: url(/src/assets/background.png);
+    height: 650px;
+    padding-top: 5%;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Majoys from '../pages/Majors'
 import Subjects from '../pages/Subjects'
 import Materials from '../pages/Materials'
 import UserProfile from '../pages/UserProfile'
+import Index from '../pages/Index'
 
 //引入VueRouter
 import VueRouter from 'vue-router'
@@ -22,6 +23,10 @@ Vue.use(VueRouter);
 //创建router实例对象，去管理一组一组的路由规则
 const router = new VueRouter({
   routes: [
+    {
+      path: '',
+      component: Index
+    },
     {
       path: '/schools',
       component: Schools
