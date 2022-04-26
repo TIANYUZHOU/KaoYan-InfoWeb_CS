2e5aae840396c0e33a73081e95d83644c3b8a71e
TIANYUZHOU <1183380395@qq.com>
2022-04-26 21:54:03 +0800

    新增-请求后台友链数据

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -18,15 +18,20 @@
         <a-col :span="8">
           <a-card title="友情链接" :bordered="false">
             <ul>
-              <li><a href="https://yz.chsi.com.cn/"> 研招网</a></li>
+              <li v-for="item in links" :key="item.id">
+                <a :href="item.url">{{ item.webTitle }}</a>
+              </li>
+              <!-- <li><a href="https://yz.chsi.com.cn/"> 研招网</a></li>
               <li><a href="https://www.kaoyan.com/"> 考研帮</a></li>
-              <li><a href="http://www.okaoyan.com/"> 考研派</a></li>
+              <li><a href="http://www.okaoyan.com/"> 考研派</a></li> -->
             </ul>
           </a-card>
         </a-col>
         <a-col :span="8">
           <a-card title="关于「上岸吧」" :bordered="false">
-            <p>「上岸吧」是一款用于计算机考研信息查询的网站系统，致力于为计算机类专业考生确定目标院校时期提供信息查询服务。</p>
+            <p>
+              「上岸吧」是一款用于计算机考研信息查询的网站系统，致力于为计算机类专业考生确定目标院校时期提供信息查询服务。
+            </p>
           </a-card>
         </a-col>
       </a-row>
@@ -45,8 +50,31 @@
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'Footer',
+    data() {
+      return {
+        links: [],
+      }
+    },
+    mounted() {
+      this.getLinks()
+    },
+    methods: {
+      getLinks() {
+        let url = 'http://127.0.0.1:8000/api/links/'
+        axios
+          .get(url)
+          .then((res) => {
+            this.links = res.data
+            console.log(res.data)
+          })
+          .catch((e) => {
+            alert(e)
+          })
+      },
+    },
   }
 </script>
 
