d96065b83d511dea44b5828754f8e2c18d2c5c7a
TIANYUZHOU <1183380395@qq.com>
2022-05-01 14:35:40 +0800

    修改-组件异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@
 import Vue from 'vue'
 
 //引入 组件
-import Schools from '../pages/Schools'
-import Majoys from '../pages/Majors'
-import Subjects from '../pages/Subjects'
-import Materials from '../pages/Materials'
-import UserProfile from '../pages/UserProfile'
-import Index from '../pages/Index'
+// import Schools from '../pages/Schools'
+// import Majoys from '../pages/Majors'
+// import Subjects from '../pages/Subjects'
+// import Materials from '../pages/Materials'
+// import UserProfile from '../pages/UserProfile'
+// import Index from '../pages/Index'
 
 //引入VueRouter
 import VueRouter from 'vue-router'
@@ -25,27 +25,33 @@ const router = new VueRouter({
   routes: [
     {
       path: '',
-      component: Index
+      // component: Index
+      component: resolve => require(['@/pages/Index'],resolve)
     },
     {
       path: '/schools',
-      component: Schools
+      // component: Schools,
+      component: resolve => require(['@/pages/Schools'],resolve)
     },
     {
       path: '/majors',
-      component: Majoys
+      // component: Majoys
+      component: resolve => require(['@/pages/Majors'],resolve)
     },
     {
       path: '/subjects',
-      component: Subjects
+      // component: Subjects
+      component: resolve => require(['@/pages/Subjects'],resolve)
     },
     {
       path: '/materials',
-      component: Materials
+      // component: Materials
+      component: resolve => require(['@/pages/Materials'],resolve)
     },
     {
       path: '/userProfile',
-      component: UserProfile
+      // component: UserProfile
+      component: resolve => require(['@/pages/UserProfile'],resolve)
     }
 
   ]
